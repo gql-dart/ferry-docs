1143467f3eb74d69e48f8f4c623bb57b9c8f7a96
smkhalsa <smkhalsa@gmail.com>
2020-10-11 12:55:48 +0200

    move fetchpolicies docs

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   mainSidebar: {
     Ferry: ['intro', 'how-it-works'],
     'Getting Started': ['setup', 'codegen'],
-    Fetching: ['queries', 'mutations', 'pagination'],
+    Fetching: ['queries', 'mutations', 'fetch-policies', 'pagination'],
     Caching: ['cache-configuration', 'cache-interaction', 'garbage-collection'],
     Flutter: ['flutter', 'flutter-operation-widget'],
     Advanced: ['custom-scalars', 'customization'],
