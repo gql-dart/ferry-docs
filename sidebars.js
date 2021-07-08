ab394672b8b5e2e6fd8e8a6964036383c818c8a9
smkhalsa <smkhalsa@gmail.com>
2021-07-08 11:39:01 +0200

    add "Structuring Queries" page

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     'Getting Started': ['setup', 'codegen'],
     Fetching: ['queries', 'mutations', 'fetch-policies', 'pagination', 'error-handling'],
     Caching: ['cache-configuration', 'cache-interaction', 'garbage-collection'],
-    Flutter: ['flutter', 'flutter-operation-widget'],
+    Flutter: ['flutter', 'flutter-operation-widget', 'structuring-queries'],
     Advanced: ['custom-scalars', 'customization'],
   },
 };
